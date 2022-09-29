c138fc2c906f8cf064b4988dedb811632a5c3755
zicoprmd <zicoprmd@gmail.com>
2022-09-29 11:30:35 +0700

    Deploy my portofolio react app to ghpages

---
+++ precache-manifest.de0fa4182d1d6c32adf9bdbcc8ab6d3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "138d95a0c57e58616469",
+    "url": "/zicoprmd-ultimateporto/static/css/main.58fe62bb.chunk.css"
+  },
+  {
+    "revision": "138d95a0c57e58616469",
+    "url": "/zicoprmd-ultimateporto/static/js/main.a97094af.chunk.js"
+  },
+  {
+    "revision": "ab4ba46eb99ce71af80d",
+    "url": "/zicoprmd-ultimateporto/static/js/runtime~main.0b7631bd.js"
+  },
+  {
+    "revision": "47deadb16dcafc1f3d0b",
+    "url": "/zicoprmd-ultimateporto/static/js/2.9431d38e.chunk.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/zicoprmd-ultimateporto/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0911aaf5cb563ec79b50baa58014e3e9",
+    "url": "/zicoprmd-ultimateporto/static/media/1a.0911aaf5.png"
+  },
+  {
+    "revision": "8512d3123d6b46fef1a25807ec930133",
+    "url": "/zicoprmd-ultimateporto/index.html"
+  }
+];
